8e74effefa758ff8a4216bedc0a3815f73c61412
Andrey Zinchenko <astrokoit@gmail.com>
2023-01-27 17:46:47 +0700

    Сделано

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {}
+}
